10c04e864606cbe6ea8d431b8a150f8305b0e9d0
TrulyNotMalware <notleebutyee@gmail.com>
2023-01-03 17:55:55 +0900

    Init : Source initialize
    initial commit
    no related issue

---
+++ src/main/java/com/msa/leftover/transfer/LeftoverApplication.java
@@ -0,0 +1,13 @@
+package com.msa.leftover.transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeftoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeftoverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msa/leftover/transfer/LeftoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.leftover.transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeftoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
